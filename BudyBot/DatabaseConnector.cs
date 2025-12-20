d625b77b89c77b51a0ffb2fdd740ce55c7c2cc1e
budywudy9 <budywudy9@gmail.com>
2025-12-20 21:53:12 +0000

    Add project files.

---
+++ BudyBot/DatabaseConnector.cs
@@ -0,0 +1,35 @@
+﻿using MySqlConnector;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BudyBot
+{
+    internal class DatabaseConnector
+    {
+        string connectionString;
+
+        public DatabaseConnector() 
+        {
+            connectionString = "connection";
+            Task.WaitAll(connect());
+        }
+
+        private async Task connect()
+        {
+            Console.WriteLine("Connecting to MariaDb...");
+
+            try
+            {
+                await using var connection = new MySqlConnection(connectionString);
+                await connection.OpenAsync();
+                Console.WriteLine("Connected to MariaDB!");
+
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ BudyBot/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using TwitchLib;
+using TwitchLib.Client;
+using TwitchLib.Client.Enums;
+using TwitchLib.Client.Events;
+using TwitchLib.Client.Extensions;
+using TwitchLib.Client.Models;
+using TwitchLib.Communication.Clients;
+using TwitchLib.Communication.Models;
+using TwitchLib.EventSub.Websockets.Client;
+
+namespace BudyBot
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Bot bot = new Bot();
+            DatabaseConnector db = new DatabaseConnector();
+            Console.ReadLine();
+           
+        }
+
+    }
+
+    
+}
